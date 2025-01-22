fb1480dc2ed29258d5d6405255ff705a183ea4c7
Rajani Empire <rajani.swe.sust@gmail.com>
2025-01-22 18:49:31 +0600

    pattern

--- PatternSqure.java
+++ PatternSqure.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Patterns {
+public class PatternSqure {
     public static void main(String args[]){
         for(int i=1;i<=4;i++){
             for(int j=1;j<=5;j++){
