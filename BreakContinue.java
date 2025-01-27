c34d37e1952d8db3345f72e9c9aeab5ff3bb26f6
Rajani Empire <rajani.swe.sust@gmail.com>
2025-01-27 23:48:11 +0600

    hguy7u

---
+++ BreakContinue.java
@@ -0,0 +1,16 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        for(int i =1;i<6;i++){
+            if(i%2==0){
+                
+                
+                continue; 
+                
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
